364b70fe4cb5546352eab639182ac2cee07d011c
Rushikesh Sawant <sawantrushikesh380@gmail.com>
2023-08-18 19:25:06 +0530

    ui changes

--- reactNew/src/views/authentication/Login.js
+++ reactNew/src/views/authentication/Login.js
@@ -50,7 +50,7 @@ const Login2 = () => {
                 subtitle={
                   <Stack direction="row" spacing={1} justifyContent="center" mt={3}>
                     <Typography color="textSecondary" variant="h6" fontWeight="500">
-                      New to Modernize?
+                      New to Rs Bank?
                     </Typography>
                     <Typography
                       component={Link}
